7ed9951a657a590f122e95a6b3b28d4d0f25624e
lucky4426 <129209495+lucky4426@users.noreply.github.com>
2024-09-12 13:30:01 +0530

    Add files via upload

---
+++ len of string.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int stringLength(char str[]) {
+    int length = 0;
+    while (str[length] != '\0') {
+        length++;
+    }
+    return length;
+}
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s", str);
+    int length = stringLength(str);
+    printf("Length of the string is: %d\n", length);
+
+    return 0;
+}
